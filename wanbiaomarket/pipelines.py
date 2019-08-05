43b84716b91b84d082a6b7267bf892f79abe7bfc
yishaolingxian <38276002+yishaolingxian@users.noreply.github.com>
2019-08-05 15:29:10 +0800

    Update pipelines.py

--- wanbiaomarket/pipelines.py
+++ wanbiaomarket/pipelines.py
@@ -5,7 +5,11 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+import logging
 
-class WanbiaomarketPipeline(object):
+logger = logging.getLogger(__name__)
+
+class LogPipeline(object):
     def process_item(self, item, spider):
+        logging.warning("*-*pipeline的warning信息*-*")
         return item
